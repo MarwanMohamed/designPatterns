daa72a9e92a38f04814fd649fbef26c9af61d6e7
MarwanMohamed <marwan.mohamed5673@gmail.com>
2017-11-17 14:31:13 +0200

    Strategy

---
+++ Strategy.php
@@ -0,0 +1,85 @@
+<?php
+
+interface OutputInterface
+{
+    public function load();
+}
+
+class SerializedArrayOutput implements OutputInterface
+{
+    public function load()
+    {
+        return serialize($arrayOfData);
+    }
+}
+
+class JsonStringOutput implements OutputInterface
+{
+    public function load()
+    {
+        return json_encode($arrayOfData);
+    }
+}
+
+class ArrayOutput implements OutputInterface
+{
+    public function load()
+    {
+        return $arrayOfData;
+    }
+}
+
+
+
+class SomeClient
+{
+    private $output;
+
+    public function setOutput(OutputInterface $outputType)
+    {
+        $this->output = $outputType;
+    }
+
+    public function loadOutput()
+    {
+        return $this->output->load();
+    }
+}
+
+
+$client = new SomeClient();
+
+// Want an array?
+$client->setOutput(new ArrayOutput());
+$data = $client->loadOutput();
+
+// Want some JSON?
+$client->setOutput(new JsonStringOutput());
+$data = $client->loadOutput();
+
+
+//////////////////////////////////////////////////////////////////////////
+
+
+interface Greeting
+{
+	public function Greeting();
+}
+
+class SayHello implements Greeting
+{
+	public function Greeting()
+	{
+		echo 'Hello !';
+	}
+}
+
+class SayWelcome implements Greeting
+{
+	public function Greeting()
+	{
+		echo 'Welcome !';
+	}
+}
+
+
